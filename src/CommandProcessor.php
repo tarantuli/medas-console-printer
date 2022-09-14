d6ad8551bed5d5e49684fa4a66b639c1f1987fdd
Patrick Morpey <patrick@morphp.nl>
2022-09-14 12:27:53 +0200

    Moved code from console to CommandProcessor and ExceptionPrinter

--- src/CommandProcessor.php
+++ src/CommandProcessor.php
@@ -5,20 +5,44 @@ declare(strict_types=1);
 namespace Medas\ConsolePrinter;
 
 use Medas\ServiceManager\Attributes\Service;
+use Medas\ServiceManager\Interfaces\Package;
 
 #[Service]
 class CommandProcessor
 {
+    private const DEFAULT_COMMAND = 'console:command-list';
+
     public function __construct(
-        private readonly CommandFinder $commandFinder,
+        private readonly CommandFinder    $commandFinder,
+        private readonly ExceptionPrinter $exceptionPrinter,
     )
     {
     }
 
     public function process(array $arguments): void
     {
-        $processor = $this->commandFinder->find($arguments[0] ?? 'console:command-list');
+        $this->addRequestedPackages($arguments);
 
-        $processor->process($arguments);
+        try {
+            $processor = $this->commandFinder->find($arguments[0] ?? self::DEFAULT_COMMAND);
+
+            $processor->process($arguments);
+        }
+        catch (\Exception|\Error $exception) {
+            $this->exceptionPrinter->print($exception);
+        }
+    }
+
+    private function addRequestedPackages(array $arguments): void
+    {
+        foreach ($arguments as $argument) {
+            if (str_starts_with($argument, '--addPackage=')) {
+                /** @var Package $class */
+                $class = substr($argument, 13);
+
+                /** @noinspection PhpParamsInspection */
+                sm()->addPackage($class::instance());
+            }
+        }
     }
 }
---
+++ src/ExceptionPrinter.php
@@ -0,0 +1,78 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Medas\ConsolePrinter;
+
+use Medas\Console\{Formats\Style, Printer, Text};
+use Medas\Core\Str;
+use Medas\ServiceManager\Attributes\Service;
+
+#[Service]
+class ExceptionPrinter
+{
+    private Printer|null $printer = null;
+    private \Error|\Exception $exception;
+
+    public function print(\Exception|\Error $exception): void
+    {
+        $this->exception = $exception;
+
+        try {
+            $this->loadPrinter();
+            $this->printVerboseExceptionInformation();
+        }
+        catch (\Exception|\Error) {
+            // Last ditch effort to print something useful
+            $this->printAnyExceptionInformation();
+        }
+    }
+
+    private function loadPrinter(): void
+    {
+        if ($this->printer === null) {
+            // Fetch the service as late as possible, here, and not in the constructor through injection.
+            // The printer itself could be the cause of the exception.
+            $this->printer = service(Printer::class);
+        }
+    }
+
+    private function printVerboseExceptionInformation(): void
+    {
+        $this->printBacktrace();
+        $this->printExceptionInformation();
+    }
+
+    private function printBacktrace(): void
+    {
+        foreach (array_reverse($this->exception->getTrace()) as $trace) {
+            $this->printer->print(new Text((isset($trace['class']) ? $trace['class'] . $trace['type'] : '') . $trace['function'] . '()'));
+
+            foreach ($trace['args'] as $i => $argument) {
+                $this->printer->print(new Text('   ' . $i . ': ' . Str::fromVariable($argument)));
+            }
+
+            if (isset($trace['file'])) {
+                $this->printer->print(new Text('      at ' . $trace['file'] . ':' . $trace['line']));
+            }
+
+            $this->printer->print();
+        }
+    }
+
+    private function printExceptionInformation(): void
+    {
+        $this->printer
+            ->print(new Text('[Exception]', Style::Bold))
+            ->print()
+            ->print(new Text('  ' . $this->exception->getMessage()))
+            ->print()
+            ->print(new Text('at ' . $this->exception->getFile() . ':' . $this->exception->getLine()))
+            ->print();
+    }
+
+    private function printAnyExceptionInformation(): void
+    {
+        echo $this->exception->getFile(), ':', $this->exception->getLine(), '  ', $this->exception->getMessage(), "\n";
+    }
+}
