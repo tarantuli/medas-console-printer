ef08f948660fbe0329fdcf7ff6964b279768e53b
Patrick Morpey <patrick@morphp.nl>
2023-08-08 10:05:05 +0200

    Updated packages

--- bootstrap.php
+++ bootstrap.php
@@ -3,14 +3,16 @@
 declare(strict_types=1);
 
 /**
- * This file is needed to test bin/console
+ * This file is needed to test bin/console.
+ *
+ * Medas\ConfigOptions\ConfigOptionsPackage is needed for the same reason, don't remove it when it fails to meet
+ * dependency checks.
  */
 
 use Medas\ConfigOptions\ConfigOptionsPackage;
 use Medas\Console\ConsolePackage;
 use Medas\ConsolePrinter\ConsolePrinterPackage;
-use Medas\ServiceManager\ServiceConfig;
-use Medas\ServiceManager\ServiceManager;
+use Medas\ServiceManager\{ServiceConfig, ServiceManager};
 
 chdir(__DIR__);
 
